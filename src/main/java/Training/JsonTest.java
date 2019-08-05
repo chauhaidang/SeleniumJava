a92f1a3cbdbb1662e69897747cbd184046c07ff8
ChauHaiDang1 <chauhaidang1@gmail.com>
2019-08-05 14:58:53 +0700

    Handle json object v1

---
+++ src/main/java/Training/JsonTest.java
@@ -0,0 +1,49 @@
+package Training;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class JsonTest {
+    public static void main(String[] args){
+        String dataType = "[{ \"Event\": \"ODR\", \"Time\": \"11/22/1996 - 03/20/1997\", \"Status\": \"DEVELOPMENT\", \"dang\": { \"sub\": [ {\"child\":\"dangchild\"} ]}}, { \"Event\": \"ODR\", \"Time\": \"11/22/1996 - 03/20/1997\", \"Status\": \"DEVELOPMENT\", \"dang\": { \"sub\": [ {\"child\":\"dangchild\"} ]}}]";
+        JsonElement jsonE = jsonParser(dataType);
+        String kq = null;
+        kq = getValueByKey(jsonE, "child", kq);
+        System.out.println("KET QUA IS: "+ kq);
+    }
+
+    public static String getValueByKey(JsonElement element, String key, String kq){
+        if(element.isJsonArray()){
+            for(JsonElement objElement : element.getAsJsonArray()){
+                kq = getValueByKey(objElement,key,kq);
+            }
+        }
+        else if(element.isJsonObject()){
+            Set<Map.Entry<String, JsonElement>> members = element.getAsJsonObject().entrySet();
+            for(Map.Entry<String, JsonElement> e: members){
+                if(!e.getKey().equalsIgnoreCase(key)) {
+                    kq = getValueByKey(e.getValue(),key, kq);
+                }
+                else{
+                    kq = e.getValue().getAsString();
+                }
+            }
+        }
+        else if(element.isJsonNull()){
+        }
+        else if(element.isJsonPrimitive()){
+        }
+        return kq;
+    }
+
+    public static JsonElement jsonParser(String dataType){
+        JsonParser parser = new JsonParser();
+        return parser.parse(dataType);
+    }
+}
