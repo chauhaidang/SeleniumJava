0dfa5ac5fe5cc12b5e06b7784d1493be4575cb51
ChauHaiDang1 <chauhaidang1@gmail.com>
2019-08-05 15:23:46 +0700

    Handle json object v4

--- src/main/java/Training/JsonTest.java
+++ src/main/java/Training/JsonTest.java
@@ -10,39 +10,40 @@ import java.util.Map;
 import java.util.Set;
 
 public class JsonTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String dataType = "[{ \"Event\": \"ODR\", \"Time\": \"11/22/1996 - 03/20/1997\", \"Status\": \"DEVELOPMENT\", \"dang\": { \"sub\": [ {\"child\":\"dangchild\"} ]}}]";
         JsonElement jsonE = jsonParser(dataType);
         String kq = null;
-        kq = getValueByKey(jsonE, "dang", kq);
-        System.out.println("KET QUA IS: "+ kq);
+        kq = getValueByKey(jsonE, "child", kq);
+        System.out.println("KET QUA IS: " + kq);
     }
 
-    public static String getValueByKey(JsonElement element, String key, String kq){
-        if(element.isJsonArray()){
-            for(JsonElement objElement : element.getAsJsonArray()){
-                kq = getValueByKey(objElement,key,kq);
+    public static String getValueByKey(JsonElement element, String key, String kq) {
+        if (element.isJsonArray()) {
+            for (JsonElement objElement : element.getAsJsonArray()) {
+                kq = getValueByKey(objElement, key, kq);
+                if (kq != null) break;
             }
-        }
-        else if(element.isJsonObject()){
+        } else if (element.isJsonObject()) {
             Set<Map.Entry<String, JsonElement>> members = element.getAsJsonObject().entrySet();
-            for(Map.Entry<String, JsonElement> e: members){
-                if(!e.getKey().equalsIgnoreCase(key)) {
-                    kq = getValueByKey(e.getValue(),key, kq);
+            for (Map.Entry<String, JsonElement> e : members) {
+                if (!e.getKey().equalsIgnoreCase(key)) {
+                    kq = getValueByKey(e.getValue(), key, kq);
+                } else {
+                    kq = e.getValue().isJsonObject() || e.getValue().isJsonArray() ? e.getValue().toString() : e.getValue().getAsString();
                 }
-                else{
-                    kq = e.getValue().isJsonObject() || e.getValue().isJsonArray() ? e.getValue().toString(): e.getValue().getAsString() ;
-                }
+                if (kq != null) break;
             }
+        } else if (element.isJsonNull()) {
+            kq = null;
         }
-        else if(element.isJsonNull()){
-        }
-        else if(element.isJsonPrimitive()){
+        else if (element.isJsonPrimitive()) {
+            kq = null;
         }
         return kq;
     }
 
-    public static JsonElement jsonParser(String dataType){
+    public static JsonElement jsonParser(String dataType) {
         JsonParser parser = new JsonParser();
         return parser.parse(dataType);
     }
