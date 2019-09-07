6473b5167f3c9a686a94f490ad5f1db64c3d1323
Đăng Châu <chauhaidang1@gmail.com>
2019-09-07 16:39:34 +0700

    Update HashMap

--- src/main/java/Training/Collection/HashMapClass.java
+++ src/main/java/Training/Collection/HashMapClass.java
@@ -1,7 +1,9 @@
 package Training.Collection;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapClass {
     public static void main(String[] args){
@@ -10,7 +12,7 @@ public class HashMapClass {
 
         //Declare
         HashMap hm2 = new HashMap();
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        HashMap<String, Integer> hm = new HashMap<>();
 
         //Adding entry (pair)
         hm.put("label1", 1);
@@ -25,11 +27,19 @@ public class HashMapClass {
         System.out.println("After removing..." + hm);
 
         //Interact with hashmap entry
-        //Each pair element in the hashmap has its type is "map.Entry"
-        //hm.entrySet(); // this is a Set of map.Entry element
+        //Each pair element in the hashmap has its type is "Map.Entry"
+        //hm.entrySet(); // this method return a Set of Map.Entry element
 
         for(Map.Entry m: hm.entrySet()){
             System.out.println(m.getKey()+ " has value: "+ m.getValue());
         }
+
+        //Get value by key
+        System.out.println(hm.get("label1"));
+
+        //Return Set of Key in the map
+        Set<String> setOfKey = hm.keySet();
+        System.out.println(setOfKey);
+
     }
 }
