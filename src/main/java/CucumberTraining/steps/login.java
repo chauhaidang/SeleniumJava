13939f6f2caa53cece6ac746f890b0145572bf79
Đăng Châu <chauhaidang1@gmail.com>
2019-09-11 15:43:14 +0700

    Add cucumber hook for each scenario

--- src/main/java/CucumberTraining/steps/login.java
+++ src/main/java/CucumberTraining/steps/login.java
@@ -1,11 +1,24 @@
 package CucumberTraining.steps;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class login {
+
+    @Before
+    public void beforeEachScenario() {
+        System.out.println("Launch browser!");
+    }
+
+    @After
+    public void afterEachScenario() {
+        System.out.println("Close browser!");
+    }
+
     @Given("^user navigate to bank app$")
     public void userNavigateToBankApp() {
         System.out.println("Given user navigate to bank app");
