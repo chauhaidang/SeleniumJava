b044518bb1ed9df30e3606a1d10c907c6c37d8e7
Đăng Châu <chauhaidang1@gmail.com>
2019-09-11 17:07:36 +0700

    Adding Extent Report

--- src/main/java/CucumberTraining/runners/runner.java
+++ src/main/java/CucumberTraining/runners/runner.java
@@ -1,9 +1,13 @@
 package CucumberTraining.runners;
 
+import com.cucumber.listener.ExtentCucumberFormatter;
 import core.helpers.Const;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
+import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -12,7 +16,20 @@ import org.junit.runner.RunWith;
         //This glues automatically is searched in classpath because they are all java class, right? So just give the relative path
         glue = {"CucumberTraining/steps"},
         monochrome = true,
-        tags = {"@smoke"},
-        plugin = {"pretty", "html:target/cucumber"})
+        tags = {"@smoke, @regression"},
+        plugin = {"pretty", "html:target/cucumber", "com.cucumber.listener.ExtentCucumberFormatter"})
 public class runner {
+
+    @BeforeClass
+    public static void setUpReport() {
+        ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+
+        //Load report config
+        ExtentCucumberFormatter.loadConfig(new File(Const.MAIN_PATH + "/CucumberTraining/configs/extent_report_config.xml"));
+        ExtentCucumberFormatter.addSystemInfo("BrowserName", "Chrome");
+        ExtentCucumberFormatter.addSystemInfo("Browser Version", "76");
+        ExtentCucumberFormatter.addSystemInfo("Selenium Version", "Not provided yet");
+
+    }
+
 }
